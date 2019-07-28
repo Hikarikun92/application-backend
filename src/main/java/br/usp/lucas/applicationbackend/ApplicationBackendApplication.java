657b890b093498112a19049ff9b7389c47b9d842
Lucas Souza <lucas_jose_92@hotmail.com>
2019-07-28 20:49:54 -0300

    Initial commit - Generated files. Class ApplicationBackendApplicationTest was deleted as we are not going to use it yet.

---
+++ src/main/java/br/usp/lucas/applicationbackend/ApplicationBackendApplication.java
@@ -0,0 +1,13 @@
+package br.usp.lucas.applicationbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationBackendApplication.class, args);
+	}
+
+}
