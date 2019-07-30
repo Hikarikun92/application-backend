dc329c811211845cc2728150d1b171a98acfe7f5
Lucas Souza <lucas_jose_92@hotmail.com>
2019-07-29 21:59:57 -0300

    Adds web services for creating, updating and deleting an user, currently with some problems described in the comments

--- src/main/java/br/usp/lucas/applicationbackend/user/UserRestController.java
+++ src/main/java/br/usp/lucas/applicationbackend/user/UserRestController.java
@@ -1,12 +1,11 @@
 package br.usp.lucas.applicationbackend.user;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,5 +27,39 @@ public class UserRestController {
     public ResponseEntity<User> getById(@PathVariable Integer id) {
         final Optional<User> optional = repository.findById(id);
         return ResponseEntity.of(optional);
+    }
+
+    /*
+    Problems with this method:
+    - We can pass an ID the body. If a user with that ID exists, it will be overwritten, which is not the intention of this method.
+    - If the ID does not exist, a new object will be created, but the ID returned in the location header might not be the correct one.
+
+    How can we solve those problems?
+     */
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<Void> create(@RequestBody User user) {
+        repository.save(user);
+        return ResponseEntity.created(URI.create("/users/" + user.getId())).build();
+    }
+
+    /*
+    Problems with this method:
+    - The ID passed in the URL must match the one in the payload, otherwise we will try to edit an object but will overwrite another.
+    - If the ID does not exist, a new object will be created. Is that correct for this service?
+     */
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping(path = "{id}", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public void update(@PathVariable Integer id, @RequestBody User user) {
+        repository.save(user);
+    }
+
+    /*
+     Problem with this method: what if the ID does not exist? The application is throwing an "Internal Server Error"!
+     */
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping(path = "{id}")
+    public void delete(@PathVariable Integer id) {
+        repository.deleteById(id);
     }
 }
